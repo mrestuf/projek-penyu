1c505003ffb4188f97e4a2af6e14666f659dba5b
nabilwafi <nabilwafi88@gmail.com>
2023-04-26 18:45:57 +0700

    debuging npm run build

--- resources/views/authentication/template.blade.php
+++ resources/views/authentication/template.blade.php
@@ -17,7 +17,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
 
   {{-- CSS AND JS WITH VITE --}}
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
   @yield('content')
--- resources/views/dashboard/user/base.blade.php
+++ resources/views/dashboard/user/base.blade.php
@@ -17,7 +17,7 @@
   {{-- STYLES --}}
   @livewireStyles
   @stack('styles')
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/sass/ap.scss', 'resources/js/app.js'])
 </head>
 <body>
   @include('layouts.header')
